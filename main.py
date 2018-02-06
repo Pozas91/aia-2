f65325a1c78585092aa5ffb420ea8d5908d30c92
David Fernández <davidfernanrod@gmail.com>
2018-02-06 17:39:12 +0100

    Included main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Importaciones de librerías requeridas
+import re
+import time
+import tkinter as tk
+import sys
+import textwrap
+import random
+from collections import Counter
+
+# Variables globales
+
+
+# Variable usada para medir los tiempos de ejecución
+start_time = time.time()
+
+
+# Tiempo de ejecución obtenido
+print("Tiempo de ejecución en segundos: --- %s seconds ---" % (time.time() - start_time))
