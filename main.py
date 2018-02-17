fe3b1e103cd9436cc9727020391613a967391daa
David Fernández <davidfernanrod@gmail.com>
2018-02-17 12:27:18 +0100

    Modificada estructura del main

--- main.py
+++ main.py
@@ -6,23 +6,45 @@ from collections import defaultdict
 from clasificador_dt import ClasificadorDT
 from utils import *
 from datasets.prestamos import clasificacion, atributos, clases, entrenamiento, validacion, prueba
-import math
 
-# Variables globales
-
-
-# Variable usada para medir los tiempos de ejecución
+# =============================================================================
+# COMIENZO - TIEMPOS DE EJECUCIÓN
+# =============================================================================
 start_time = comienzo_tiempo_ejecucion()
 
+# =============================================================================
+# INICIALIZACIÓN EL CLASIFICADOR DT
+# =============================================================================
 clasificador = ClasificadorDT(clasificacion, clases, atributos)
-clasificador.entrena(entrenamiento)
-clasificador.evalua(prueba)
-#print(clasificador.clasifica(entrenamiento))
-#print(clasificador.clasifica(validacion))
-#print(clasificador.clasifica(prueba))
-#clasificador.imprime()
-#entropia(clasificador.get_arbol().distr)
-#entropia_media_ponderada(clasificador.get_arbol().distr)
 
-# Tiempo de ejecución obtenido
+# =============================================================================
+# INFORMACIÓN UTILIZADA DEL DATASET
+# =============================================================================
+print("Clasificación: " + clasificacion)
+print("Atributos: [{0}]".format(', '.join(map(str, atributos))))
+print("Clases: [{0}]".format(', '.join(map(str, clases))))
+print("Conjunto de entrenamiento: [{0}]".format(', '.join(map(str, entrenamiento))))
+print("Conjunto de validacion: [{0}]".format(', '.join(map(str, validacion))))
+print("Conjunto de prueba: [{0}]".format(', '.join(map(str, prueba))))
+
+print("*************************************************")
+
+# =============================================================================
+# ENTRENAMOS
+# =============================================================================
+clasificador.entrena(entrenamiento)
+
+# =============================================================================
+# EVALUAMOS
+# =============================================================================
+clasificador.evalua(entrenamiento)
+
+# =============================================================================
+# CLASIFICAMOS
+# =============================================================================
+clasificador.clasifica(entrenamiento)
+
+# =============================================================================
+# FINAL - TIEMPOS DE EJECUCIÓN
+# =============================================================================
 tiempo_ejecucion_obtenido(start_time)
